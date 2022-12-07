49e82529f1c76eef9431fb3329f1893046a9be76
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-07 21:44:32 +0300

    решение 34

--- homework034/Program.cs
+++ homework034/Program.cs
@@ -5,3 +5,39 @@
 [345, 897, 568, 234] -> 2
 */
 
+// метод инициализации массива
+int[] InitArray(int length)
+{
+   int[] array = new int[length]; // объявляем новый массив
+   Random rnd = new Random();
+   for (int i = 0; i < length; i++) // заполняем массив рандомными числами от -9 до 9
+   {
+      array[i] = rnd.Next(100, 1000);
+   }
+   return array; // возвращаем массив
+}
+
+// метод печати массива на экран
+void PrintArray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+      Console.Write($"{array[i]} ");
+   }
+   Console.WriteLine();
+}
+
+int Count(int[] array)
+{
+   int count = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] % 2 == 0)
+         count++;
+   }
+   return count;
+}
+
+int[] arr = InitArray(10);
+PrintArray(arr);
+Console.WriteLine($"Количество четных элементов в массиве = {Count(arr)}");
